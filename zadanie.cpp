52dff927327b47889786989af2f2b6d7dd0a25e0
lelovics <lelovics.mirtill@gmail.com>
2025-04-04 16:43:40 +0200

    pocet vsetkych slov

--- zadanie.cpp
+++ zadanie.cpp
@@ -3,11 +3,26 @@
 
 unsigned int pocet_tokenov(char *str)
 {
+	int i;
+	unsigned int pocet;
+	
+	i=0;
+	pocet=0;
+	
+	while(str[i]!=0)
+	{
+		if(str[i]==' '|| str[i]=='\n'|| str[i]=='\t')
+		{
+			pocet++;
+		}
+	i++;
+	}
+return(pocet);
 }
 
 int main(void)
 {
-	char text[]="";
+	char text[]="prvy test";
 	unsigned int vysledok = pocet_tokenov(text);
 	
 	printf("pocet tokenov: %d\n ",vysledok);
